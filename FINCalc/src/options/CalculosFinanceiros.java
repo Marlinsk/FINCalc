93c02664cbad1628acfda663b0cd318e5be30c79
Marlon <Marlonnlmx@outlook.com>
2021-09-22 08:05:19 -0300

    Update

--- FINCalc/src/options/CalculosFinanceiros.java
+++ FINCalc/src/options/CalculosFinanceiros.java
@@ -36,7 +36,7 @@ public class CalculosFinanceiros extends Carteira {
 		System.out.println(df.format(economiaTotalAno));
 	}
 	
-	public static void calculoFinanceiro(double valorSalario, double dinheiroEconimzado, double metaDeGasto, int mes) {
+	public static void calculoSobra(double valorSalario, double dinheiroEconimzado, double metaDeGasto, int mes) {
 		
 		BigDecimal sobra = new BigDecimal(valorSalario - (dinheiroEconimzado + metaDeGasto));
 		BigDecimal quantidadeMeses = new BigDecimal(mes);
@@ -50,21 +50,6 @@ public class CalculosFinanceiros extends Carteira {
 		DecimalFormat df = new DecimalFormat(padraoBR, dfs);
 		System.out.println(df.format(sobra));
 		System.out.println(df.format(acumulo));
-		
-	}
-	
-	public static void calculoDeMetas(double metaDeEconomia, int anos) {
-		
-		int calc1 = anos * 12;
-		BigDecimal calc2 = new BigDecimal(metaDeEconomia * calc1);
-		
-		DecimalFormatSymbols dfs = new DecimalFormatSymbols(new Locale("pt", "Brazil"));
-		dfs.setDecimalSeparator(',');
-		dfs.setGroupingSeparator('.');
-		
-		String padraoBR = "###,###.##";
-		DecimalFormat df = new DecimalFormat(padraoBR, dfs);
-		System.out.println(df.format(calc2));
 		
 	}
 	
