50a9e29408f20a661d12bf64b2ca73bcaacffc62
Marlon <Marlonnlmx@outlook.com>
2021-09-21 21:12:48 -0300

    Update to class Investimentos.java

--- FINCalc/src/options/Investimentos.java
+++ FINCalc/src/options/Investimentos.java
@@ -8,8 +8,7 @@ import java.util.Locale;
 import entidade.Carteira;
 
 public class Investimentos extends Carteira {
-	
-	// Calcular Rentabilidade de uma ação com taxa pré-fixada
+
 	public static void calcularRentabilidadecomTaxaPreFixada(double valorAplicado, double taxaPreFixada, int prazoAno) {
         
 		double variavelaux1 = (1 + taxaPreFixada / 100);
@@ -25,9 +24,9 @@ public class Investimentos extends Carteira {
 		String padraoBR = "###,###.##";
 		DecimalFormat df = new DecimalFormat(padraoBR, dfs);
 		System.out.println(df.format(valorfuturo));
+		
 	}
-
-	// Calcular Rentabilidade de uma ação com taxa pós-fixada
+     
     public static void calcularRentabilidadecomTaxaPosFixada(double valorAplicado, double rendimentoAnual, double taxaPosFixada, int prazoAno) {
     	
     	double variavelaux1 = ( 1 + (taxaPosFixada / 100) * (rendimentoAnual/100));
@@ -44,15 +43,14 @@ public class Investimentos extends Carteira {
 		DecimalFormat df = new DecimalFormat(padraoBR, dfs);
     	System.out.println(df.format(valorFututo));
 	}
-	
-    // Calcular Rentabilidade de uma ação com taxa IPCA
+    
     public static void calcularRentabilidadecomTaxaIPCA(double valorAplicado, double rendimentoAnual, double taxaIPCA, int prazoAno) {
     	
     	double variavelaux1 = ( 1 + (taxaIPCA / 100) * (rendimentoAnual/100));
     	int variavelaux2 = prazoAno;
     	double formula = Math.pow(variavelaux1, variavelaux2);
     	
-    	BigDecimal valorfututo = new BigDecimal(valorAplicado * formula);
+    	BigDecimal valorfututo3 = new BigDecimal(valorAplicado * formula);
     	
     	DecimalFormatSymbols dfs = new DecimalFormatSymbols(new Locale("pt", "Brazil"));
 		dfs.setDecimalSeparator(',');
@@ -60,21 +58,22 @@ public class Investimentos extends Carteira {
 		
 		String padraoBR = "###,###.##";
 		DecimalFormat df = new DecimalFormat(padraoBR, dfs);
-    	System.out.println(df.format(valorfututo));
+    	System.out.println(df.format(valorfututo3));
+    
     }
 	
-    // Calculo de rentabilidade acumulada de uma ação
-    public static void rentabilidadeAcumulada(double valorAplicado, double valorAtual) {
+    public void rentabilidadeAcumulada(double valorAplicado, double valorAtual) {
     	
-    	BigDecimal rentabilidade = new BigDecimal(((valorAtual / valorAplicado) - 1) * 100);
+    	BigDecimal rentabilidadeAtual = new BigDecimal(((valorAtual / valorAplicado) - 1) * 100);
     	
     	DecimalFormatSymbols dfs = new DecimalFormatSymbols(new Locale("pt", "Brazil"));
 		dfs.setDecimalSeparator(',');
 		dfs.setGroupingSeparator('.');
-		
+			
 		String padraoBR = "###,###.##";
 		DecimalFormat df = new DecimalFormat(padraoBR, dfs);
-    	System.out.println(df.format(rentabilidade));
+    	System.out.println(df.format(rentabilidadeAtual));
+        
     }
-    
+
 }
