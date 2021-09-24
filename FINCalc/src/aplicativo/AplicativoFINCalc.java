723f6f496774f0633e8c6a0e232e04fe9b9cbc17
Marlon <Marlonnlmx@outlook.com>
2021-09-23 22:37:07 -0300

    Banned class

--- FINCalc/src/aplicativo/AplicativoFINCalc.java
+++ FINCalc/src/aplicativo/AplicativoFINCalc.java
@@ -1,10 +1,37 @@
 package aplicativo;
 
+import entidade.Carteira;
+import options.Investimentos;
+
 public class AplicativoFINCalc {
 
 	public static void main(String[] args) {
-		
-
+		   /*
+           Investimentos iv = new Investimentos();
+           
+           iv.setValorAplicado(195.66);
+           
+           iv.setPrazoAno(3);
+           //iv.setValorAnualPorcentagem(3.56);
+           
+           iv.setTaxaPreFixada(7.20);
+           iv.setTaxaPosFixada(4.76);
+           iv.setTaxaIPCA(5);
+           
+           double valoraplicado = iv.getValorAplicado();
+           
+           int prazoano = iv.getPrazoAno();
+           //double valorAnual = iv.getValorAnualPorcentagem();
+           
+           double taxaPreFixada = iv.getTaxaPreFixada();
+           double taxaPosFixada = iv.getTaxaPosFixada();
+           double taxaIPCA = iv.getTaxaIPCA();
+           */
+           /*
+           iv.calcularRentabilidadecomTaxaPreFixada(valoraplicado, taxaPreFixada, prazoano);
+           iv.calcularRentabilidadecomTaxaPosFixada(valoraplicado, valorAnual, taxaPosFixada, prazoano);
+           iv.calcularRentabilidadecomTaxaIPCA(valoraplicado, valorAnual, taxaIPCA, prazoano);
+           */
 	}
 
 }
