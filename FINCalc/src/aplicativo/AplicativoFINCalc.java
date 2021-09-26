4484293cc310f154ac2930249718f2c5555d4933
Marlon <Marlonnlmx@outlook.com>
2021-09-26 15:12:02 -0300

    Launch new code to AplicativoFINCalc.java

--- FINCalc/src/aplicativo/AplicativoFINCalc.java
+++ FINCalc/src/aplicativo/AplicativoFINCalc.java
@@ -8,7 +8,7 @@ import options.Investimentos;
 
 public class AplicativoFINCalc {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 
 		String menu = menuCalculadora();
 
@@ -18,7 +18,7 @@ public class AplicativoFINCalc {
 			case "plan":
 				planejamentoParticao();
 				;
-			case "calc invest":
+			case "calcinvest":
 				calculadoraInvestimentos();
 				;
 			}
@@ -34,15 +34,19 @@ public class AplicativoFINCalc {
 
 		String comando;
 
-		System.out.println("APLICATIVO FINCalc");
+		System.out.println("\n" + "----------------------------------------------------------");
+
+		System.out.println("\nAPLICATIVO FINCalc");
 
 		System.out.println("\nCOMANDOS: ");
 
 		System.out.println("\nplan: acessa a calculadora de planejamento financeiro;");
-		System.out.println("calc invest: acessa a calculadora de investimentos;");
+		System.out.println("calcinvest: acessa a calculadora de investimentos;");
 
 		System.out.println("\nESCOLHA UM DOS COMANDOS LISTADOS E DIGITE-O: ");
 		comando = scan.next();
+
+		System.out.println("\n" + "----------------------------------------------------------");
 
 		return comando;
 	}
@@ -53,7 +57,7 @@ public class AplicativoFINCalc {
 
 		Carteira carteira = new Carteira();
 		PlanejamentoFinanceiro planRenda = new PlanejamentoFinanceiro();
-		
+
 		double valorRendaMensal;
 		int opcao;
 
@@ -66,9 +70,9 @@ public class AplicativoFINCalc {
 
 		System.out.println("Escolha uma das formas de organizar a renda mensal listadas abaixo:");
 
-		System.out.println("\n1 = 50% 30% 20%");
-		System.out.println("2 = 40% 30% 30%");
-		System.out.println("3 = 60% 20% 10% 10%");
+		System.out.println("\n1 = 50% / 30% / 20% ;");
+		System.out.println("2 = 40% / 30% / 30% ;");
+		System.out.println("3 = 60% / 20% / 10% / 10% ;");
 
 		System.out.println("\nDigite o número da opção desejada: ");
 		opcao = scan.nextInt();
@@ -78,16 +82,118 @@ public class AplicativoFINCalc {
 		}
 
 		if (opcao == 2) {
-            planRenda.regraFinanceira2(valorRendaMensal);  
+			planRenda.regraFinanceira2(valorRendaMensal);
 		}
 
 		if (opcao == 3) {
-           planRenda.regraFinanceira3(valorRendaMensal); 
+			planRenda.regraFinanceira3(valorRendaMensal);
 		}
 	}
 
 	private static void calculadoraInvestimentos() {
-        
+
+		Scanner scan = new Scanner(System.in);
+		
+		Investimentos inv = new Investimentos();
+
+		double valorInvestido;
+		double taxaPreFixada;
+		double taxaPosFixada;
+		double taxaIPCA;
+		double rendimentoAplicacao;
+		double rendimentoAtual;
+		int prazoAno;
+
+		int opcao;
+
+		System.out.println("\nCalcule aqui a rentabilidade das suas ações");
+
+		System.out.println("\nEscolha o tipo de investimento: ");
+		System.out.println("\n1 - Investimento com Taxa Pre-fixada;");
+		System.out.println("2 - Investimento com Taxa Pós-fixada;");
+		System.out.println("3 - Investimento com Taxa IPCA ;");
+		System.out.println("4 - Calcular rentabilidade atual de um investimento");
+
+		System.out.println("\nDigite o número da opção desejada: ");
+		opcao = scan.nextInt();
+
+		if (opcao == 1) {
+
+			System.out.println("\nDigite a porcentagem da taxa de rendimento pré-fixado da aplicação:");
+
+			System.out.println("(*Observação: Não é necessário escrever a procentagem com o símbolo % e o valor divido por 100)");
+			taxaPreFixada = scan.nextDouble();
+
+			System.out.println("\nDigite o prazo em anos:");
+			prazoAno = scan.nextInt();
+
+			System.out.println("\nDigite o valor investido nesta aplicação:");
+			valorInvestido = scan.nextDouble();
+
+			System.out.println("\nNo final do prazo, você terá em caixa");
+			inv.calcularRentabilidadecomTaxaPreFixada(taxaPreFixada, prazoAno, valorInvestido);
+
+		}
+
+		if (opcao == 2) {
+            
+			System.out.println("\nDigite a porcentagem da taxa de rendimento pós-fixada da aplicação");
+			
+			System.out.println("(*Observação: Não é necessário escrever a procentagem com o símbolo % e o valor divido por 100):");
+			taxaPosFixada = scan.nextDouble();
+			
+			System.out.println("\nDigite a porcentagem de rendimento anual da aplicação");
+			
+			System.out.println("(*Observação: Não é necessário escrever a procentagem com o símbolo % e o valor divido por 100):");
+			rendimentoAplicacao = scan.nextDouble();
+			
+			System.out.println("\nDigite o prazo em anos:");
+			prazoAno = scan.nextInt();
+			
+			System.out.println("\nDigite o valor investido nesta aplicação:");
+			valorInvestido = scan.nextDouble();
+			
+			System.out.println("\nNo final do prazo, você terá em caixa");
+			inv.calcularRentabilidadecomTaxaPosFixada(taxaPosFixada, rendimentoAplicacao, prazoAno, valorInvestido);
+			
+		}
+
+		if (opcao == 3) {
+           
+			System.out.println("\nDigite a porcentagem da taxa de rendimento IPCA da aplicação");
+			
+			System.out.println("(*Observação: Não é necessário escrever a procentagem com o símbolo % e o valor divido por 100):");
+			taxaIPCA = scan.nextDouble();
+			
+			System.out.println("\nDigite a porcentagem de rendimento anual da aplicação");
+			
+			System.out.println("(*Observação: Não é necessário escrever a procentagem com o símbolo % e o valor divido por 100):");
+			rendimentoAplicacao = scan.nextDouble();
+			
+			System.out.println("\nDigite o prazo em anos:");
+			prazoAno = scan.nextInt();
+			
+			System.out.println("\nDigite o valor investido nesta aplicação:");
+			valorInvestido = scan.nextDouble();
+			
+			System.out.println("\nNo final do prazo, você terá em caixa");
+			inv.calcularRentabilidadecomTaxaIPCA(taxaIPCA, rendimentoAplicacao, prazoAno, valorInvestido);
+			
+		}
+		
+		if (opcao == 4) {
+            
+			System.out.println("\nDigite o valor aplicado:");
+			valorInvestido = scan.nextDouble();
+			
+			System.out.println("\nDigite o valor de rendimento atual na aplicação:");
+			rendimentoAtual = scan.nextDouble();
+			
+			System.out.println("\nSua aplicação rendeu:");
+			inv.rentabilidadeAcumulada(valorInvestido, rendimentoAtual);
+			
+		}
+
 	}
 
 }
